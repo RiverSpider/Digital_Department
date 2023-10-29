403a13991ce4383dd54f62be6e60ab4d88fe9366
RiverSpider <raevskih8888@mail.ru>
2023-10-29 21:25:27 +0600

    Добавил Шрифты

---
+++ app/src/main/java/com/example/dotainstall/ui/theme/fonts.kt
@@ -0,0 +1,12 @@
+package com.example.dotainstall.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.dotainstall.R
+
+internal val FontFamily = FontFamily(
+    Font(R.font.sk_modernist_bold_font, weight = FontWeight.Bold),
+    Font(R.font.sk_modernist_mono_font, weight = FontWeight.Medium),
+    Font(R.font.sk_modernist_regular_font)
+)
