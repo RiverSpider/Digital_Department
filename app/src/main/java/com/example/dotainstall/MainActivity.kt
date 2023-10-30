91c1c26657c4eb336382a2b4c9a93ed5bd0fc799
RiverSpider <raevskih8888@mail.ru>
2023-10-30 18:05:58 +0600

    Добавил logo и background. Так же исправил header

--- app/src/main/java/com/example/dotainstall/MainActivity.kt
+++ app/src/main/java/com/example/dotainstall/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.dotainstall
 
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
+import com.example.dotainstall.ui.theme.BackGround
 import com.example.dotainstall.ui.theme.DotaInstallTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,18 +26,49 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
-            DotaHeader()
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF050B18))
+            ) {
+                DotaHeader()
+                DotaLogo(
+                    imageResId = R.drawable.logo,
+                    size = 80.dp,
+                    primaryBorderSize = 1.dp,
+                    primaryBorderColor = androidx.compose.ui.graphics.Color.DarkGray,
+                    cornerRadiusprimary = 8.dp,
+                    secondaryBorderSize = 10.dp,
+                    secondaryBorderColor = androidx.compose.ui.graphics.Color.Black,
+                    cornerRadiussecondary = 17.dp,
+                    x = 17.dp,
+                    y = 275.dp
+                )
+            }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    DotaHeader()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF050B18))
+    ) {
+        DotaHeader()
+        DotaLogo(
+            imageResId = R.drawable.logo,
+            size = 80.dp,
+            primaryBorderSize = 1.dp,
+            primaryBorderColor = androidx.compose.ui.graphics.Color.DarkGray,
+            cornerRadiusprimary = 8.dp,
+            secondaryBorderSize = 10.dp,
+            secondaryBorderColor = androidx.compose.ui.graphics.Color.Black,
+            cornerRadiussecondary = 17.dp,
+            x = 17.dp,
+            y = 275.dp
+        )
+    }
 }
